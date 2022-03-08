5674bea14c112105728bf101405eb9d672075c83
gitofcharan <sricharan7861@gmail.com>
2022-03-08 13:22:02 +0530

    one line added

--- gitfile.py
+++ gitfile.py
@@ -1,6 +1,7 @@
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('Hi PyCharm')
+    print('this is charan')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 a = 10
