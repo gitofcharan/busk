5eb6ac10edcd03d56c8dc2fdd155d96d75a43b3f
gitofcharan <101165502+gitofcharan@users.noreply.github.com>
2022-03-08 13:08:02 +0530

    Add files via upload

---
+++ gitfile.py
@@ -0,0 +1,9 @@
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Hi PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a = 10
+b = 20
+c = 39
+print(a+b+c)
